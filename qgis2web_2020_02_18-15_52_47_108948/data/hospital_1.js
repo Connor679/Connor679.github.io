dc08465e2c0c9a1805a9eedb386c7f04c054a4ea
Connor679 <60405971+Connor679@users.noreply.github.com>
2020-02-18 17:45:33 -0500

    Add files via upload

---
+++ qgis2web_2020_02_18-15_52_47_108948/data/hospital_1.js
@@ -0,0 +1,22 @@
+var json_hospital_1 = {
+"type": "FeatureCollection",
+"name": "hospital_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ADDRESS": "600 N. Wolfe St.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21287", "NAME": "John Hopkins Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.592282280093499, 39.2972016030944 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "827 Linden Ave.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21201", "NAME": "Maryland General Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.620508557033119, 39.299728845670892 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "2000 W. Baltimore St.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21223", "NAME": "Bon Secours Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.649063514299769, 39.288340719301708 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "2401 W. Belvedere Ave.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21215", "NAME": "Sinai Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.662108375759573, 39.352566850604219 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "3001 S. Hanover St.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21225", "NAME": "Harbor Hospital Center" }, "geometry": { "type": "Point", "coordinates": [ -76.613913981631669, 39.251871119361866 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "900 S. Caton Ave.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21229", "NAME": "St. Agnes Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.671867490025321, 39.272354562338393 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "201 E. University PW.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21218", "NAME": "Union Memorial Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.61429886267301, 39.329207528014521 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "5601 Lockraven Bd.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21239", "NAME": "Good Samaritan Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.587288459022488, 39.358313451084754 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "4940 Eastern Ave.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21224", "NAME": "John Hopkins Bayview Medical Center" }, "geometry": { "type": "Point", "coordinates": [ -76.548599431479317, 39.292603114043359 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "301 St. Paul St.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21202", "NAME": "Mercy Medical Cen." }, "geometry": { "type": "Point", "coordinates": [ -76.613301678351803, 39.292848581366229 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "22 S. Greene St.", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21201", "NAME": "University of Maryland Medical Center" }, "geometry": { "type": "Point", "coordinates": [ -76.624765121579856, 39.287916795200907 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "10 N. Greene St", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21201", "NAME": "VA Medical Center" }, "geometry": { "type": "Point", "coordinates": [ -76.624475342522175, 39.289703470580733 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "2200 Kernan Dr", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21207", "NAME": "Kernan Hospital" }, "geometry": { "type": "Point", "coordinates": [ -76.708791052629579, 39.314706105316105 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "1708 W ROGERS AVE", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21209", "NAME": "MOUNT WASHINGTON PEDIATRIC HOSPITAL" }, "geometry": { "type": "Point", "coordinates": [ -76.653496136146686, 39.363191287928899 ] } },
+{ "type": "Feature", "properties": { "ADDRESS": "611 S CHARLES ST", "CITY": "Baltimore", "STATE": "MD", "ZIPCODE": "21230", "NAME": "UNIVERSITY SPECIALTY HOSPITAL" }, "geometry": { "type": "Point", "coordinates": [ -76.614180200731326, 39.281369237337955 ] } }
+]
+}
